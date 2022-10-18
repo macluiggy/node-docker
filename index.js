a1246891c1166f1942deaab849df109649c55605
luiggy macias <ferrinluiggy@gmail.com>
2022-10-17 20:28:57 -0500

    sss

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send('<h1 style="color: red;">Hello World!</h1>');
 });
 
 app.listen(port, () => {
