627cbeb788c4c13d31dd2788fa9baaf719cd99f1
luiggy macias <ferrinluiggy@gmail.com>
2022-10-19 19:47:05 -0500

    ddd

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
 const app = express();
-
+const NODE_ENV = process.env.NODE_ENV
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send('<h1 style="color: red;">Hello World!</h1>');
+  res.send(`<h1 style="color: red;">Hello World! you are in ${NODE_ENV} mode</h1>`);
 });
 
 app.listen(port, () => {
