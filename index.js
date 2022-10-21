98c5c6c1805742b706121f99e6b670a669a460af
luiggy macias <ferrinluiggy@gmail.com>
2022-10-20 20:19:56 -0500

    adding networks

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+const mongoose = require('mongoose');
 const express = require("express");
 
 const app = express();
-const NODE_ENV = process.env.NODE_ENV
+
+mongoose
+  .connect("mongodb://root:root@mongo:27017/?authSource=admin", {})
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err)); 
+const NODE_ENV = process.env.NODE_ENV;
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send(`<h1 style="color: red;">Hello World! you are in ${NODE_ENV} mode</h1>`);
+  res.send(
+    `<h1 style="color: red;">Hello World! you are in ${NODE_ENV} mode</h1>`
+  );
 });
 
 app.listen(port, () => {
