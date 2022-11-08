42d69a1cc4df2263eeaa5504585a3fcc8a90e867
luiggy macias <ferrinluiggy@gmail.com>
2022-11-07 19:37:32 -0500

    vv

--- index.js
+++ index.js
@@ -9,19 +9,35 @@ const {
 
 const app = express();
 
-const mongoUrl = process.env.MONGO_URI || `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+const mongoUrl =
+  process.env.MONGO_URI ||
+  `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
-mongoose
-  .connect(
-    mongoUrl
-    //   {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // }
-  )
+// mongoose
+//   .connect(
+//     mongoUrl
+//     //   {
+//     //   useNewUrlParser: true,
+//     //   useUnifiedTopology: true,
+//     // }
+//   )
 
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.log(err));
+
+const connectWithRetry = () => {
+  console.log("MongoDB connection with retry");
+  mongoose
+    .connect(mongoUrl)
+    .then(() => console.log("MongoDB is connected"))
+    .catch((err) => {
+      console.log(err);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
+
 const NODE_ENV = process.env.NODE_ENV;
 const port = process.env.PORT || 3000;
 
